a73c972b03554eea5cf5fa797cd73a2c5af17a9b
Talha Riaz <muhammadtalha1005@gmail.com>
2023-05-10 02:30:21 +0500

    how are you

--- db.js
+++ db.js
@@ -2,10 +2,10 @@
 
 const mongoose = require('mongoose');
 const mongooseURI='mongodb+srv://talhariaz:talhariaz@cluster0.k2itfyk.mongodb.net/portel?retryWrites=true&w=majority'
-const connectToMongo = () => {
-  mongoose.connect(mongooseURI)
-    .then(() => console.log('Connected to MongoDB!'))
-    .catch(error => console.error('Error connecting to MongoDB:', error));
+const connectToMongo=()=>{
+  mongoose.connect(mongooseURI,()=>{
+      console.log("connected to mongo successfull")
+  })
 }
-
-module.exports= connectToMongo;
+mongoose.set('strictQuery', false);
+module.exports= connectToMongo;;
